af6225efee130c9838b9bbb1f8046dec87b6256b
aimers1975 <amy_hindman@yahoo.com>
2015-06-21 13:19:08 -0500

    Rewrote protocol to simplify.

--- hw2/work/q2/MainTest.java
+++ hw2/work/q2/MainTest.java
@@ -4,18 +4,43 @@ public class MainTest {
 		//ArrayList<Person> line = new ArrayList<Person>();
 		SyncBathroomProtocol protocol = new SyncBathroomProtocol();
 		Person firstmale = new Person(false,protocol, 5000);
+		Person thmale = new Person(false,protocol, 7000);
 		Person firstfemale = new Person(true,protocol, 10000);
 		Person secfemale = new Person(true,protocol, 5000);
+		Person thfemale = new Person(true, protocol,10000);
 		Person secmale = new Person(false,protocol, 2000);
+		Person fofemale = new Person(true, protocol,5000);
+		Person fifemale = new Person(true, protocol,6000);
+		Person sifemale = new Person(true, protocol,6000);
+		Person sefemale = new Person(true, protocol,6000);
+		Person fomale = new Person(false,protocol, 2000);
+		Person fimale = new Person(false,protocol, 2000);
+
 		firstmale.start();
+		thmale.start();
 		firstfemale.start();
 		secfemale.start();
+		thfemale.start();
 		secmale.start();
+		fofemale.start();
+		fifemale.start();
+		sifemale.start();
+		sefemale.start();
+		fomale.start();
+		fimale.start();
 		try {
 			firstmale.join();
+			thmale.join();
 			firstfemale.join();
 			secfemale.join();
+			thfemale.join();
 			secmale.join();
+			fofemale.join();
+			fifemale.join();
+			sifemale.join();
+			sefemale.join();
+			fomale.join();
+			fimale.join();			
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
--- hw2/work/q2/SyncBathroomProtocol.java
+++ hw2/work/q2/SyncBathroomProtocol.java
@@ -4,119 +4,90 @@
 
 public class SyncBathroomProtocol implements BathroomProtocol {
 
-
-  	boolean doorClosed;
-  	int occupantCount;
-  	volatile boolean entering;
-  	int femaleCheck;
-  	int maleCheck;
-
-	public SyncBathroomProtocol() {
-  		doorClosed = false;
-  		occupantCount = 0;  // positive for femails/negative for males
-  		entering = false;
-  		femaleCheck = 0;
-  		maleCheck = 0;		
-	}
+  int femaleCount;
+  int maleCount;
+  int maleWait;
+  int femaleWait;
 
 
+  public SyncBathroomProtocol() {
+      femaleCount = 0;
+      femaleWait = 0;
+      maleCount = 0;
+      maleWait = 0;
+  }
 
   public void enterMale() {
-  		closeDoor(false);
+      closeDoor(false);
 
   }
 
   public void leaveMale() {
-  		openDoor(false);
+      openDoor(false);
 
   }
 
   public void enterFemale() {
-  		closeDoor(true);
+      closeDoor(true);
   }
 
   public void leaveFemale() {
-  		openDoor(true);
+      openDoor(true);
   }
 
   public synchronized void closeDoor(boolean female) {
-  	try {
-  		if(doorClosed) {
-  			System.out.println("The door is closed");
-  		} else {
-  			System.out.println("The door is opened");
-  		}
+      boolean setWait = false;
+      try {
+        if(female) {
+            System.out.println("Female trying to enter. Female count: " + femaleCount + " Female wait: " + femaleWait + " Male count: " + maleCount + " Male wait: " + maleWait);
+            while(maleCount > 0 || maleWait > 0) {
+                while(maleWait > 0) { wait();}
+                if(!setWait) { 
+                    femaleWait++; 
+                    setWait=true;
+                    System.out.println("Female waiting. Female count: " + femaleCount + " Female wait: " + femaleWait + " Male count: " + maleCount + " Male wait: " + maleWait);
+                }
+                wait();
+            }
+            femaleCount++;
+            if(setWait) {femaleWait--;}
+            System.out.println("Female entered. Female count: " + femaleCount + " Female wait: " + femaleWait + " Male count: " + maleCount + " Male wait: " + maleWait);
+            notify();
 
-  		if(female) {
-  			while(doorClosed && (occupantCount < 0)) { 
-  				System.out.println("Female is waiting.");
-  				wait();
-  			}
-  			System.out.println("Female door is open, trying to enter.");
-  			while(entering) { 
-  				System.out.println("Female is waiting for someone to finish entering.");
-  				wait();
-  			}
-  			entering = true;
-  			System.out.println("Female got into the bathroom.");
-  			occupantCount++;
-  			femaleCheck++;
-  			System.out.println("Male check is: " + maleCheck + "Female check is: " + femaleCheck);
-  			doorClosed = true;
-  			entering = false;
-  			notifyAll();
-  		} else {
-  			while(doorClosed && (occupantCount > 0)) { 
-  				System.out.println("Male is waiting for the door to open.");
-  				wait();
-  			}
-  			System.out.println("Male door is open, trying to enter.");
-  			while(entering) { 
-  				System.out.println("Male is waiting for someone to finish entering.");
-  				wait();
-  			}
-  			entering = true;
-  			System.out.println("Male got into the bathroom.");
-  			occupantCount--;
-  			maleCheck++;
-  			System.out.println("Male check is: " + maleCheck + "Female check is: " + femaleCheck);
-  			doorClosed = true;
-  			entering = false;
-  			notifyAll();  			
-  		}
-  	} catch (InterruptedException e) {
-  		e.printStackTrace();
-  	}
+        } else {
+            System.out.println("Male trying to enter. Female count: " + femaleCount + " Female wait: " + femaleWait + " Male count: " + maleCount + " Male wait: " + maleWait);
+            while(femaleCount > 0 || femaleWait > 0) {
+                while(femaleWait > 0) { wait();}
+                if(!setWait) {
+                    setWait=true;
+                    System.out.println("Male waiting. Female count: " + femaleCount + " Female wait: " + femaleWait + " Male count: " + maleCount + " Male wait: " + maleWait); 
+                    maleWait++; 
+                }
+                wait();
+            }
+            maleCount++;
+            if(setWait) {maleWait--;}
+            System.out.println("Male entered. Female count: " + femaleCount + " Female wait: " + femaleWait + " Male count: " + maleCount + " Male wait: " + maleWait);
+            notify();
+        }
+      } catch(InterruptedException e) {
+          e.printStackTrace();
+
+      }
+
   }
 
   public synchronized void openDoor(boolean female) {
-  	try {
-  		if(female && (occupantCount > 0)) {
-  			while(entering) { wait();}
-  			entering = true;
-  			occupantCount--;
-  			femaleCheck--;
-  			System.out.println("Male check is: " + maleCheck + "Female check is: " + femaleCheck);
-  			if(occupantCount == 0) {
-  				doorClosed = false;
-  			}
-  			entering = false;
-  			notify();
-  		} else if(!female && occupantCount < 0) {
-  			while(entering) { wait();}
-  			entering = true;
-  			occupantCount ++;
-  			maleCheck--;
-  			System.out.println("Male check is: " + maleCheck + "Female check is: " + femaleCheck);
-  			if(occupantCount == 0) {
-  				doorClosed = false;
-  			}
-  			entering = false;
-  			notify();
-  		}
-  	} catch (InterruptedException e) {
-  		e.printStackTrace();
-  	}
+      if(female) {
+          femaleCount--;
+          System.out.println("Female exited. Female count: " + femaleCount + " Female wait: " + femaleWait + " Male count: " + maleCount + "Male wait: " + maleWait);
+          notify();
+      } else {
+          maleCount--;
+          System.out.println("Male exited. Female count: " + femaleCount + " Female wait: " + femaleWait + " Male count: " + maleCount + "Male wait: " + maleWait);
+          notify();
+      }
   }
+
 
 }
