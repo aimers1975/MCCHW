ef7fdb313a9c9aee8ff6509513f92da7b67dbab3
aimers1975 <amy_hindman@yahoo.com>
2015-06-27 22:06:11 -0500

    Matrix mult may be working.

--- hw3/matrixmult.cpp
+++ hw3/matrixmult.cpp
@@ -6,8 +6,24 @@
 #include <sstream>
 using namespace std;
 
-/*bool MatrixMult(int rowA, int colA, double* A, int rowB, int colB, double* B,
-	double* C, int T) {
+// Want to reuse code to init matrix
+void initMatrix(double** matrix, int rm, int cm, bool zerod, ifstream &filein) {
+	for (int i = 0; i < rm; i++) {
+		matrix[i] = new double[cm];
+		for (int j = 0; j < cm; j++) {
+			if (zerod) {
+				matrix[i][j] = 0;
+			}
+			else {
+				filein >> matrix[i][j];
+			}
+			//cout << "Row: " << i << " col: " << j << " is read: " << matrix[i][j] << "\n";
+		}
+	}
+}
+
+bool MatrixMult(int rowA, int colA, double** A, int rowB, int colB, double** B,
+	double** C, int T) {
 	// TODO: Implement your parallel multiplication for two matrices of doubles
 	// by using OpenMP
 	// parameter rowA: indicates the number of rows of the matrix A
@@ -19,16 +35,37 @@ using namespace std;
 	// parameter C: indicates the matrix C, which is the results of A x B
 	// parameter T: indicates the number of threads
 	// return true if A and B can be multiplied; otherwise, return false 
-	if (ROWA != COLB) {
-	cout << "The matix cannot be mult.";
-	cin.get();
-	return false;
+	if (rowA != colB) {
+		cout << "The matix cannot be mult. \nRow A = " << rowA << " and Col B = " << colB << "\n";
+		cin.get();
+		return false;
 	}
 	else {
-	cout << "The matix can be mult.";
-	cin.get();
+		cout << "The matix can be mult. \nRow A = " << rowA << " and Col B = " << colB << " \n";
+
+		omp_set_num_threads(T);  
+
+		double temp = 0.0;
+		#pragma omp parallel for reduction (+:temp)
+		for (int row = 0; row < rowA; row++) { 
+			for (int col = 0; col < colB; col++) {
+				// Multiply the row of A by the column of B to get the row, column of product.
+				temp = 0.0;
+				for (int inner = 0; inner < colA; inner++) {
+					//int ID = omp_get_thread_num();
+					//cout << "The thread id: " << ID << "\n";
+					//cout << "Multiplying: " << "A[" << row << "][" << inner << "]" << " by " << "B[" << inner << "][" << col << "] toget C[" << row << "][" << col << "]" "\n";
+					temp += A[row][inner] * B[inner][col];
+				}
+				C[row][col] = temp;
+				cout << C[row][col] << "  ";
+			}
+			cout << "\n";
+		}
+		cin.get();
+		return true;
 	}
-}*/
+}
 
 
 int main(int argc, const char *argv[]) {
@@ -67,62 +104,22 @@ int main(int argc, const char *argv[]) {
 	inf2 >> ROWB >> COLB;
 	double** A;
 	double** B;
+	double** C;
 
 	A = new double*[ROWA];
-	for (int i = 0; i < ROWA; i++) {
-		A[i] = new double[COLA];
-		for (int j = 0; j < COLA; j++) {
-			inf >> A[i][j];
-			cout << "Row: " << i << " col: " << j << " is read: " << A[i][j] << "\n";
-		}
-	}
-
 	B = new double*[ROWB];
-	for (int i = 0; i < ROWB; i++) {
-		B[i] = new double[COLB];
-		for (int j = 0; j < COLB; j++) {
-			inf2 >> B[i][j];
-			cout << "Row: " << i << " col: " << j << " is read: " << B[i][j] << "\n";
-		}
-	}
-
-	/*int aMatrix[3][2] = {{1, 4}, {2, 5}, {3, 6}};
-    int bMatrix[2][3] = {{7, 8, 9}, {10, 11, 12}};
-    int product[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
-
-    for (int row = 0; row < 3; row++) {
-        for (int col = 0; col < 3; col++) {
-            // Multiply the row of A by the column of B to get the row, column of product.
-            for (int inner = 0; inner < 2; inner++) {
-                product[row][col] += aMatrix[row][inner] * bMatrix[inner][col];
-            }
-            std::cout << product[row][col] << "  ";
-        }
-        std::cout << "\n";
-    }
-}	*/
-
+	C = new double*[ROWA];
+	initMatrix(A, ROWA, COLA, false, inf);
+	initMatrix(B, ROWB, COLB, false, inf2);
+	initMatrix(C, ROWA, COLB, true, inf2);
 
 
 	// TODO: Initialize the necessary data
-
-	/*if (MatrixMult(ROWA, COLA, A, ROWB, COLB, B, C, T)) {
-		// TODO: Output the results
+	if (MatrixMult(ROWA, COLA, A, ROWB, COLB, B, C, T)) {
+		cout << "The matrix can be multiplied" << endl;
 	}
 	else {
 		cout << "the colA != rowB MatrixMult return false" << endl;
-	}
-	return 0;*/
-	if (ROWA != COLB) {
-		cout << "The matix cannot be mult.";
-		cout << "ROWA: " << ROWA << " and COLB is: " << COLB << "\n";
-		cin.get();
-		return false;
-	}
-	else {
-		cout << "The matix can be mult.";
-		cout << "ROWA: " << ROWA << " and COLB is: " << COLB << "\n";
-		cin.get();
 	}
 	return 0;
 }
