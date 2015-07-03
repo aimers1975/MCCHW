feb2abee33d3cdae2c3eb34d1b4e81706e3f5374
aimers1975 <amy_hindman@yahoo.com>
2015-07-02 20:34:36 -0500

    Anderson lock implementation.

--- hw3/q2/ALock.java
+++ hw3/q2/ALock.java
@@ -1,18 +1,36 @@
+import java.util.concurrent.atomic.AtomicInteger;
 // TODO 
 // Implement Anderson’s array-based lock
 
 public class ALock implements MyLock {
+    ThreadLocal<Integer> mySlotIndex = new ThreadLocal<Integer>(){
+          protected Integer initialValue() {
+               return 0;
+          }
+     };
+     AtomicInteger tail;
+     boolean[] flag;
+     int size;
     public ALock(int numThread) {
       // TODO: initialize your algorithm
+      size = numThread;
+      tail = new AtomicInteger(0);
+      flag = new boolean[size];
+      flag[0] = true;
     }
 
     @Override
     public void lock(int myId) {
-      // TODO: the locking algorithm
+          int slot = tail.getAndIncrement() % size;
+          mySlotIndex.set(slot);
+          while (! flag[slot]) {};
     }
 
     @Override
     public void unlock(int myId) {
-      // TODO: the unlocking algorithm
+          int slot = mySlotIndex.get();
+          flag[slot] = false;
+          flag[(slot + 1) % size] = true;      // TODO: the unlocking algorithm
     }
 }
+
