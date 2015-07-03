d91be2d164a7005867d0e0971193b4ab679891b7
aimers1975 <amy_hindman@yahoo.com>
2015-07-03 05:43:12 -0500

    Added MCS lock implementation

--- hw3/q2/MCSLock.java
+++ hw3/q2/MCSLock.java
@@ -1,39 +1,56 @@
 // TODO
 // Implement the MCS Lock 
 
+import java.util.concurrent.atomic.*;
+
 public class MCSLock implements MyLock {
 
-	//(shared) tailNode;
-	//(ThreadLocal) myNode;
-	//(ThreadLocal) predessor;
+	volatile AtomicReference<Node> tailNode;	  
+	volatile ThreadLocal<Node> myNode;
+
 
 	public class Node {
-		boolean Locked = false;
+		boolean locked = false;
 		Node next = null;
 	}
 
     public MCSLock(int numThread) {
+    	tailNode = new AtomicReference<Node>();
+    	myNode = new ThreadLocal<Node>(){ protected Node initialValue() { return new Node();}};
     }
 
     @Override
     public void lock(int myId) {
-	//predecessor = tailNode.getAndSet(myNode);
-	//If(predecessor != null) {
-	//	myNode.locked = true;
-	//	pred.next = myNode;
-	//	while(myNode locked) { wait();}  // wait on local memory vs shared
-
+    	Node thisNode = myNode.get();
+		Node predecessor = tailNode.getAndSet(thisNode);
+		if(predecessor != null) {
+			thisNode.locked = true;
+			predecessor.next = thisNode;
+			while(thisNode.locked) { 
+				try {
+					Thread.sleep(1000);
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
+			} 
+		} 
     }
 
     @Override
     public void unlock(int myId) {
-	//If(myNode.next == null) {
-	//	If(tail.compareAndSet(myNode,null))
-	//		Return;
-	//	While(myNode.next == null) { wait();}  
-	//}
-	//myNode.next.locked = false;
-	//myNode.next = null;
-
+    	Node thisNode = myNode.get();
+		if(thisNode.next == null) {
+			if(tailNode.compareAndSet(thisNode,null))
+				return;
+			while(thisNode.next == null) { 
+				try {
+					Thread.sleep(1000);
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
+			}  
+		}
+		thisNode.next.locked = false;
+		thisNode.next = null;
     }
 }
